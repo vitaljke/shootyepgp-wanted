341bca132a09a05bdb9e11a3d4c7a895b3e7fd2c
vitaljke <54172059+vitaljke@users.noreply.github.com>
2025-06-19 22:05:00 +0300

    Add files via upload

---
+++ bosslist.lua
@@ -0,0 +1,87 @@
+local zones = {
+  ["Molten Core"] = {
+    {"Lucifron", 10},
+    {"Magmadar", 10},
+    {"Gehennas", 10},
+    {"Garr", 10},
+    {"Shazzrah", 10},
+    {"Baron Geddon", 10},
+    {"Golemagg the Incinerator", 10},
+    {"Sulfuron Harbinger", 10},
+    {"Majordomo Executus", 10, {
+      "The Eye of Divinity",
+      "Ancient Petrified Leaf",
+      "Core Forged Greaves",
+      "Sash of Whispered Secrets",
+      "Wild Growth Spaulders",
+      "Wristguards of True Flight",
+      "Cauterizing Band",
+      "Finkle's Lava Dredger",
+      "Core Hound Tooth",
+      "Gloves of the Hypnotic Flame",
+      "Fireproof Cloak",
+      "Fireguard Shoulders"
+    }},
+    {"Ragnaros", 10}
+  },
+
+  ["Blackwing Lair"] = {
+    {"Razorgore the Untamed", 15},
+    {"Vaelastrasz the Corrupt", 15},
+    {"Broodlord Lashlayer", 15},
+    {"Firemaw", 15},
+    {"Ebonroc", 15},
+    {"Flamegor", 15},
+    {"Chromaggus", 15},
+    {"Nefarian", 15}
+  },
+
+  ["Onyxia's Lair"] = {
+    {"Onyxia", 20}
+  },
+
+  ["Ahn'Qiraj"] = {
+    {"The Prophet Skeram", 12},
+    {"Vem", 12},
+    {"Lord Kri", 12},
+    {"Princess Yauj", 12},
+    {"Battle Guard Sartura", 12},
+    {"Fankriss the Unyielding", 12},
+    {"Viscidus", 12},
+    {"Princess Huhuran", 12},
+    {"Emperor Vek'lor", 12},
+    {"Emperor Vek'nilash", 12},
+    {"Ouro", 12},
+    {"C'Thun", 18}
+  },
+
+  ["Naxxramas"] = {
+    {"Patchwerk", 20},
+    {"Grobbulus", 20},
+    {"Gluth", 20},
+    {"Thaddius", 20},
+    {"Anub'Rekhan", 20},
+    {"Grand Widow Faerlina", 20},
+    {"Maexxna", 20},
+    {"Noth the Plaguebringer", 20},
+    {"Heigan the Unclean", 20},
+    {"Loatheb", 20},
+    {"Instructor Razuvious", 20},
+    {"Gothik the Harvester", 20},
+    {"Four Horseman", 20, {
+      "Desecrated Breastplate",
+      "Desecrated Tunic",
+      "Desecrated Robe"
+    }},
+  },
+
+  ["Sapphiron's Lair"] = {
+    {"Sapphiron", 20},
+  },
+  
+  ["Kel'Thuzad Chamber"] = {
+    {"Kel'Thuzad", 20}
+  }
+}
+
+sepgp.boss_list = zones
